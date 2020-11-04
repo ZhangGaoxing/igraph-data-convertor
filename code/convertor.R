bc5056be4f5419a7193033bf12b7a6d7d09796bb
Zhang Yuexin <zhangyuexin121@live.cn>
2020-11-04 10:17:24 +0800

    save connection

--- code/convertor.R
+++ code/convertor.R
@@ -4,6 +4,13 @@ graph_edges <- read.csv(file = "./csv/hep-ph/edges.csv", header = TRUE)
 
 graph <- graph_from_data_frame(graph_edges, directed = FALSE)
 graph <- simplify(graph, remove.multiple = TRUE, remove.loops = TRUE)
+
+if (!is.connected(graph)) {
+    com <- components(graph)
+    gr <- groups(com)
+    graph <- induced_subgraph(graph, gr[[1]])
+}
+
 E(graph)$weight <- runif(n = length(E(graph)), min = 0, max = 1)
 
 save(graph, file = "./graph/HepPh.RData")
